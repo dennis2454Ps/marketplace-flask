115ad1be4b5916d005f50a4e7d1f8e5325156060
dennis2454Ps <76782124@certus.edu.pe>
2024-10-19 11:31:52 -0500

    ultimo cambio

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Habilita CORS en todas las rutas
-
-# Simular una base de datos de productos
+CORS(app)  
 productos = []
 
 # Ruta para crear un producto
